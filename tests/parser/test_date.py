216f0c571237640b32d1f684787ec49ee7f0ea9f
Justin Vanderhooft <justinvdhooft@gmail.com>
2022-03-25 16:36:29 -0400

    fixup! fixup! feat: add date logic

--- tests/parser/test_date.py
+++ tests/parser/test_date.py
@@ -1,17 +1,24 @@
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
 from freezegun import freeze_time
 
 from melnor_bluetooth.parser.date import _time_offset, get_timestamp
 
+no_dst = datetime(2022, 3, 11, 0, 0, 0, tzinfo=ZoneInfo("America/Detroit"))
+dst = datetime(2022, 3, 13, 0, 0, 0, tzinfo=ZoneInfo("America/Detroit"))
+
 
 class TestDateTester:
-    @freeze_time("2022-03-11 03:21:34", tz_offset=-4)
+    @freeze_time(no_dst)
     def test_time_offset_no_dst(self):
         assert _time_offset() == 46800
 
-    @freeze_time("2022-03-11 03:21:34", tz_offset=-4)
+    @freeze_time(dst)
     def test_time_offset_dst(self):
         assert _time_offset() == 46800
 
-    @freeze_time("2022-03-11 03:21:34", tz_offset=-4)
-    def test_get_timestamp_bytes(self):
-        assert get_timestamp() == 700266094
+    # DST is largely irrelevant to this computation as long as the offset is correct which we validate above
+    @freeze_time(no_dst)
+    def test_get_timestamp_no(self):
+        assert get_timestamp() == 700272000

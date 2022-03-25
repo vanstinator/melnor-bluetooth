8864ed54fda47e7b33d9924975f7a8d39ec60da3
Justin Vanderhooft <justinvdhooft@gmail.com>
2022-03-25 16:20:44 -0400

    feat: add date logic

--- tests/parser/test_date.py
+++ tests/parser/test_date.py
@@ -3,15 +3,13 @@ from freezegun import freeze_time
 from melnor_bluetooth.parser.date import _time_offset, get_timestamp
 
 
+@freeze_time("2022-03-11", tz_offset=-4)
 class TestDateTester:
-    @freeze_time("2022-03-11")
     def test_time_offset_no_dst(self):
         assert _time_offset() == 46800
 
-    @freeze_time("2022-04-11")
     def test_time_offset_dst(self):
         assert _time_offset() == 46800
 
-    @freeze_time("2022-03-11")
     def test_get_timestamp_bytes(self):
         assert get_timestamp() == 700254000
